bfd4e86b4a3597a9d837172fda4de56a75103841
tensho9408 <tenshoohashi@gmail.com>
2021-03-31 06:51:43 +0900

    zn_version

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ from collections import Counter
 from textblob import TextBlob
 import neattext
 
+# NLP PKgs For ZH
+import jieba 
+from jieba import analyse
+
+
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -18,19 +23,34 @@ nltk.download('averaged_perceptron_tagger')
 import pandas as pd
 
 # Data Viz Pkgs 
+from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt 
+
 import seaborn as sns 
 import altair as alt 
 plt.style.use("ggplot") 
 
 # Fx 
 
-def plot_wordcloud(docx):
-    mywordcloud = WordCloud().generate(docx)
-    fig = plt.figure(figsize=(20,10))
-    plt.imshow(mywordcloud, interpolation="bilinear")
-    plt.axis("off")
-    st.pyplot(fig)
+def plot_wordcloud(docx,lang=None):
+    # Font Path and Property 
+    if lang == "ZN" or "zn":
+        font_path = r"./data/SimHei.ttf"
+        font = FontProperties(fname= font_path,size=16)
+        mywordcloud = WordCloud(font_path=font_path,background_color="PAPAYAWHIP").generate(docx)
+
+        fig = plt.figure(figsize=(20,10))
+        plt.imshow(mywordcloud, interpolation="bilinear")
+        plt.axis("off")
+        st.pyplot(fig)
+
+    else: 
+        mywordcloud = WordCloud().generate(docx)
+
+        fig = plt.figure(figsize=(20,10))
+        plt.imshow(mywordcloud, interpolation="bilinear")
+        plt.axis("off")
+        st.pyplot(fig)
 
 def plot_word_freq(docx,num=10):
     word_freq_list = Counter(docx.split())
@@ -117,6 +137,96 @@ TAGS =  {
     'None' : 'off'
 }
 
+TAGS_ZN ={
+    'r'   : 'green',
+    'n'   : 'green',
+    'nr'  : 'green',
+    'nz'  : 'green',
+    'PER' : 'green',
+    'a'   : 'blue',
+    'm'  : 'blue',
+    'c'  : 'blue',
+    'VBP'  : 'blue',
+    'VBZ'  : 'blue',
+    'ns'   : 'red',
+    'nt'  : 'red',
+    'nv'  : 'red',
+    't'   : 'cyan',
+    'TIME'  : 'cyan',
+    'd'  : 'cyan',
+    'p'   : 'darkwhite',
+    'vn'  : 'darkyellow',
+    'ORG' : 'magenta',
+    'LOC' : 'magenta',
+    'q'   : 'black',
+    'u'   : 'black',
+    'ad'   : 'black',
+    'v'  : 'black',
+    'vd'   : 'black',
+    'an'  : 'black',
+    'xc'  : 'black',
+    'f'   : 'yellow',
+    'w'   : 'yellow',
+    'nw'   : 'yellow',
+}
+
+TAGS_ZN_T ={
+    'green': 'r',
+    'green': 'n',
+    'green' :'nr',
+    'green': 'nz',
+    'green': 'PER',
+    'blue':  'a',
+    'blue':  'm',
+    'blue':  'c',
+    'blue':  'VBP',
+    'blue':  'VBZ',
+    'red':   'ns',
+    'red':   'nt',
+    'red':   'nv',
+    'cyan':  't',
+    'cyan':  'TIME',
+    'cyan':  'd',
+    'darkwhite': 'p',
+    'darkyellow': 'vn',
+    'magenta': 'ORG',
+    'magenta': 'LOC',
+    'black':  'q',
+    'black':  'u',
+    'black':  'ad',
+    'black':  'v',
+    'black':  'vd',
+    'black':  'an',
+    'black':  'xc',
+    'yellow': 'f',
+    'yellow': 'w',
+    'yellow': 'nw',
+}
+
+
+def mytag_visualizer_zn(tagge_docx):
+    
+    colored_text = []
+    token_ = []
+    color_tag_ = []
+    tag_color_ = []
+    for token, tag in tagge_docx.items():
+        if tag in TAGS_ZN.keys():
+            toeken = token
+            color_for_tag = TAGS_ZN.get(tag)
+            tag_for_color = TAGS_ZN_T.get(color_for_tag)
+            result =  '<span style="color:{}">{}</span>'.format(color_for_tag,token)
+            colored_text.append(result)
+            token_.append(token)
+            color_tag_.append(color_for_tag)
+            tag_color_.append(tag_for_color)
+
+    token_color_df = pd.DataFrame([token_, color_tag_,tag_color_]).T
+    token_color_df.columns = ["tokens","colors","tags"]
+
+    st.write(token_color_df)
+    result = "".join(colored_text)
+    return result
 
 def mytag_visualizer(tagge_docx):
     colored_text = []
@@ -124,25 +234,75 @@ def mytag_visualizer(tagge_docx):
         if i[1] in TAGS.keys():
             token = i[0]
             color_for_tag = TAGS.get(i[1])
-            result = '<span style="color:{}">{}"</span>'.format(color_for_tag,token)
+            result = '<span style="color:{}">{}</span>'.format(color_for_tag,token)
             colored_text.append(result)
     result = " ".join(colored_text)
-    #print(result)
+    
     return result
 
+# Split word into to list 
+def preprocess_tokens_list(text):
+    # Load Stopwords 
+    with open("./stopwords/stopwords.txt","r") as f:
+        stopwords_zn = [word.strip() for word in f ] 
+
+    seg_list = jieba.cut(text,cut_all=True,) # return list 
+    preprossed_tokens_list = " ".join([token for token in seg_list if token not in stopwords_zn])
+    
+    return preprossed_tokens_list
+
+def plot_mendelhall_curve_zn(docx):
+    raw_test_token_list = jieba.cut(docx,cut_all=True)
+    raw_test_token_str = " ".join(raw_test_token_list)
+    # Call fx 
+    plot_mendelhall_curve(raw_test_token_str)
+
+def most_common_keyword_extracter_idf_zn(docx,common=10):
+    coommon_keyword_idf_list = analyse.extract_tags(docx,topK=common,withWeight=True)
+    coommon_keyword_idf_df = pd.DataFrame(coommon_keyword_idf_list,columns=["Word Most Common","Inverse Document Frequency"])
+    
+    st.dataframe(coommon_keyword_idf_df)
+
+def pos_jieba_zn(raw_text):
+    pseg = jieba.posseg.cut(raw_text)
+
+    words_tags_list = []
+
+    for i in pseg:
+        words_tag_str = (i.__str__().decode("utf-8"))
+        words_tags_list_ = (words_tag_str.split("/"))
+    
+    
+        #words_tags_list.extend(words_tags_list_)
+        for w_t in (words_tags_list_):
+            a = "".join(list(w_t))
+            words_tags_list.append(a)
+    
+    df = pd.DataFrame(words_tags_list,columns=["pos"])
+    words = df[df["pos"].index % 2  == 0 ].values.tolist()
+    tags = df[df["pos"].index % 2 == 1 ].values.tolist()
+
+    words_tags_dict = {}
+    for w,t  in zip(words,tags):
+       
+        words_tags_dict[w[0]] = t[0]
+
+    return words_tags_dict 
 
 
 
 def main():
     st.title("Text Analysis App")
 
-    menu = ["Home","About"]
+    menu = ["English","Chinese","Japanese"]
     Choice = st.sidebar.selectbox("Menu",menu)
 
-    if Choice == "Home":
-        st.subheader("Home")
+    if Choice == "English":
+        st.subheader("EN")
         # Text Area 
         raw_text = st.text_area("Enter Text Here")
+
+        
         if st.button("Submit"):
             if len(raw_text) > 2:
                 st.success("Processing")
@@ -151,12 +311,13 @@ def main():
             else:
                 st.write("Enter Text") 
         # if raw_text is not None:
-        #    st.sucess(raw_text)
-
+    
         # Layout 
         col1,col2 = st.beta_columns(2)
         processed_text = nfx.remove_stopwords(raw_text)
         processed_text_deep = nfx.remove_special_characters(processed_text)
+
+       
 
         with col1:
             with st.beta_expander("Original Text"):
@@ -198,9 +359,85 @@ def main():
                     if len(raw_text) > 2:   
                         plot_mendelhall_curve(raw_text)  
                 
-    else:
-        st.subheader("About")
+    elif Choice == "Chinese":
+        st.subheader("中文")
 
+        # Text Area 
+        raw_text = st.text_area("请输入文本")
+    
+        # Preprocess
+        processed_text = preprocess_tokens_list(raw_text)
+
+        #pseg_list_tup = {words:tags for words, tags in pseg }
+        #st.write((words_tag_dic))
+       
+        # submit bottom 
+        if st.button("Submit"):
+            if len(raw_text) > 2:
+                st.success("Processing")
+            elif len(raw_text) == 1:
+                st.warning("Insufficient Text,Minimum is 2") 
+            else:
+                st.warning("请输入文本") 
+         # Basic tf-idf
+        
+        if len(processed_text) > 1 :
+            st.subheader("逆向文件频率")
+            most_common_keyword_extracter_idf_zn(processed_text)
+            st.text("——————————————————"*5)
+    
+        # Layout 
+        col1,col2 = st.beta_columns(2)
+        with col1:
+            with st.beta_expander("原文本"):
+                st.write(raw_text)
+        with col1:
+            with st.beta_expander("划分词类"):
+                if len(raw_text) > 1: 
+                    st.success("Part of Speech")
+                    words_pos_tagged_dict = pos_jieba_zn(raw_text)
+                    
+                    #st.write(words_pos_tagged_dict)
+                    tagged_span_color_html = mytag_visualizer_zn(words_pos_tagged_dict)
+                    stc.html(tagged_span_color_html,scrolling=True)
+                else: st.warning("请输入文本")
+
+        with col1:
+            with st.beta_expander("词出现频率展示图"):
+                max_limit = len(processed_text.split())
+                if len(processed_text) > 1:
+                    try:
+                        num_of_tokens = st.number_input("num of tokens",10,max_value=max_limit)
+                        plot_word_freq(processed_text,num_of_tokens)
+                    except:st.info("请输入足够的文本")
+                else: st.warning("请输入文本")
+
+        with col2:
+            with st.beta_expander("处理后的文本"):
+                if len(processed_text) >1:
+                    st.write(processed_text)
+                else:st.warning("已输入的文本皆焉停用词，请再输入其他文本")   
+
+        with col2:
+            with st.beta_expander("展示词云"):
+                if len(processed_text) >1 :
+                    st.success("Word Plot")
+                    plot_wordcloud(processed_text,lang="zn")
+                else: st.warning("请输入文本")
+                
+
+        with col2:
+            with st.beta_expander("词长度和出现频率展示图"):
+                if len(raw_text) > 1:
+                    st.success("Mendehall Curve")
+                    plot_mendelhall_curve_zn(raw_text)
+
+                else: st.warning("请输入文字")
+            
+
+                
+    else:
+        st.subheader("JA")
 
 
 if __name__ == "__main__":
